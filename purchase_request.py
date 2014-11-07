5148a4ad0c0b46dc2775a970709ab59e0e1b59c6
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:49:01 +0100

    Cleanup tests

--- purchase_request.py
+++ purchase_request.py
@@ -1,6 +1,6 @@
-#This file is part stock_supply_best_supplier module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part stock_supply_best_supplier module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from trytond.pool import PoolMeta
 
--- tests/test_stock_supply_best_supplier.py
+++ tests/test_stock_supply_best_supplier.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
-#This file is part stock_supply_best_supplier module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part stock_supply_best_supplier module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends
 
 
 class StockSupplyBestSupplierTestCase(unittest.TestCase):
-    '''
-    Test Stock Supply Best Supplier module.
-    '''
+    'Test Stock Supply Best Supplier module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('stock_supply_best_supplier')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
